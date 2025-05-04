4bb2530472cb156efec48d8aa424d20ce2d69012
cryan-code1 <cryan.code@gmail.com>
2025-05-04 13:44:58 -0300

    chore(backend): habilitando CORS para permitir chamadas do frontend

--- Portifolio/Portfolio.Backend/Program.cs
+++ Portifolio/Portfolio.Backend/Program.cs
@@ -7,7 +7,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader());
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
